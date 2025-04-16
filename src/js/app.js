291cbe2acf97edd2f85294e34fbb3a53cb8edec0
Anabell <anabekk726@gmail.com>
2025-04-16 00:05:58 +0000

    finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { left, right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -29,18 +30,34 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let position = variables.socialMediaPosition;
+  if (variables.socialMediaPosition == position - left)
+    position = "<div class='position'></div>";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+           ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>
+          ${variables.name === null ? "firts" : variables.name},
+          ${variables.lastName === null ? "Last" : variables.lastName}</h1>
+          <h2>${variables.role === null ? "Role" : variables.role}</h2>
+          <h3>${variables.city === null ? "City," : variables.city},
+              ${variables.country === null ? "Country" : variables.country}
+          </h3>
+          <ul class=${position}>       
+            <li><a href=${
+              variables.twitter === null ? "" : variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github === null ? "" : variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin === null ? "" : variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram === null ? "" : variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
